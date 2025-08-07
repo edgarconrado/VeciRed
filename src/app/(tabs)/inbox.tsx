17ef4508a6728c88d4ef3907ded9d1c1b0cc4645
Edgar Rafael Conrado Hernandez <edgar.conrado@oreillyauto.com>
2025-08-07 15:26:35 -0600

    Initial project

---
+++ src/app/(tabs)/inbox.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function InoxScreen(){
+    return(
+        <View>
+            <Text>
+                Inbox Screen
+            </Text>
+        </View>
+    )
+}
